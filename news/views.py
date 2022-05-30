3e24d05d87f27385feef797298e6d346e951fe7e
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-05-30 10:53:21 +0300

    Create a subscribe template that sends email notifications

---
+++ news/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse, Http404,HttpResponseRedirect
+from django.shortcuts import render
+from .forms import NewsLetterForm
+from .email import send_welcome_email
+from .models import *
+# Create your views here.
+def subscribe(request):
+#........
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+
+            recipient = NewsLetterRecipients(name = name,email =email)
+            recipient.save()
+            send_welcome_email(name,email)
+
+            HttpResponseRedirect('subscribe.html')
+    else:
+        form = NewsLetterForm()
+    return render(request, 'subscribe.html', {"letterForm":form})
